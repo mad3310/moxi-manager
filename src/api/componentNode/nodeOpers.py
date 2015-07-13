ba980f9ccbd4d3eb3a8ecae1cb876370a76db4f8
xuyanwei <xuyanwei@CNBJWK5172G6V.letv.local>
2015-07-13 17:48:00 +0800

    1、去掉所有zookeeper的导入和实例化zookeeper的地方 2、nodeOpers中去掉create方法（不必要） 3、nodeOpers中不再导入需要zk的UUID的方法 4、nodeOpers中不再导入configFileOpers中的方法

--- src/api/componentNode/nodeOpers.py
+++ src/api/componentNode/nodeOpers.py
@@ -7,52 +7,20 @@ Created on Mar 13, 2015
 import os,logging
 
 from tornado.options import options
-from utils import retrieve_node_name
 from utils.invokeCommand import InvokeCommand
 from common.abstractOpers import AbstractOpers
 from utils.exceptions import UserVisiableException
-from utils import getClusterUUID
-from utils.configFileOpers import ConfigFileOpers
 
 
 class NodeOpers(AbstractOpers):
     '''
     classdocs
     '''
-    confOpers = ConfigFileOpers()
     invokeCommand = InvokeCommand()
     def __init__(self):
         '''
         Constructor
         '''
-        
-    def create(self, params):
-        if params == {} or params is None:
-            raise UserVisiableException("please set the componentNode info!")
-        
-        dataNodeInternalPort = params.get('dataNodeInternalPort')
-        if dataNodeInternalPort is not None:
-            raise UserVisiableException("no need to set the dataNodeInternalPort param!")
-            
-        zkOper = Common_ZkOpers()
-        
-        local_uuid = getClusterUUID()
-        existCluster = zkOper.existCluster(local_uuid)
-        if not existCluster:
-            raise UserVisiableException("sync componentCluster info error! please check if sync uuid is right!")
-            
-        params.setdefault("dataNodeInternalPort", options.port)
-        dataNodeExternalPort = params.get('dataNodeExternalPort')
-        if dataNodeExternalPort is None or '' == dataNodeExternalPort:
-            params.setdefault("dataNodeExternalPort", options.port)
-        
-        self.confOpers.setValue(options.data_node_property, params)
-        dataNodeProprs = self.confOpers.getValue(options.data_node_property)
-        zkOper.writeDataNodeInfo(local_uuid, dataNodeProprs)
-
-        result = {}
-        result.setdefault("message", "Configuration on this componentNode has been done successfully")    
-        return result
     
     def start(self):
         ret_val = os.system(options.start_moxi)
